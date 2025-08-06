b383eac63059936a84f6c3e6144d55d22f501939
Aquib Ahmed <aquibahmed21@gmail.com>
2025-08-06 16:32:32 +0530

    Keeping Server alive using setInterval

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,16 @@ import express, { Request, Response } from 'express';
 const app = express();
 const port = 3000;
 
+const refreshIntervalMinute = 10;
+
+setInterval(() => {
+  fetch('http://localhost:3000/')
+    .then(response => response.text())
+    .then(data => console.log('Response:', data))
+    .catch(error => console.error('Error:', error));
+}, refreshIntervalMinute * 60 * 1000);
+
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello from Express in TypeScript!');
 });
